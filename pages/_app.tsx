a173eca7d85f2726e50336712c546ba5691f505d
George Kakarlis <georgekakarlis@yahoo.com>
2022-07-14 21:52:38 +0200

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import apolloClient from '../lib/apollo'
+import Layout from '../components/layout'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return 
+     <ApolloProvider client={apolloClient}> 
+       <Layout>
+         <Component {...pageProps} />
+       </Layout> 
+     </ApolloProvider>
 }
 
 export default MyApp
